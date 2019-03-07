39e6cd638ee050c69f6693f4f5dc57ee232dc4e9
ben <moon791017@gmail.com>
2019-03-07 08:58:23 +0800

    OK

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -2,15 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  mode: 'development',
   entry: {
     app: path.resolve(__dirname, 'src/index.js'),
     print: path.resolve(__dirname, 'src/print.js')
   },
-  devtool: 'inline-source-map',
-  devServer: {
-        contentBase: './dist'
-      },
+ 
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
