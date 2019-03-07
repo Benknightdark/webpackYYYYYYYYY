8ff7b7d3a5ac1cbb3168035ff232fb1a0d0c5674
ben <moon791017@gmail.com>
2019-03-07 10:15:46 +0800

    OK

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -4,8 +4,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/index.js'),
-    print: path.resolve(__dirname, 'src/print.js'),
-    another: path.resolve(__dirname, 'src/cc.js')
   },
  
   plugins: [
