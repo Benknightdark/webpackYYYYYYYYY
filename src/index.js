95917fac1b9fe79f8e74e719319f2182880287dc
ben <moon791017@gmail.com>
2019-03-07 08:52:24 +0800

    aa

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import _ from 'lodash';
-import {appendChild} from "three";
-import * as gui from "dat.gui";
-import * as stats from 'stats-js';
+// import {appendChild} from "three";
+// import * as gui from "dat.gui";
+// import * as stats from 'stats-js';
+import printMe from './print.js';
 
 
 function component() {
     let element = document.createElement('div');
-  
+    let btn = document.createElement('button');
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');  
-    
+    btn.innerHTML = 'Click me and check the console!';
+   btn.onclick = printMe;
+
+   element.appendChild(btn);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+  mode: 'development',
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+    print: path.resolve(__dirname, 'src/print.js')
   },
- 
+  devtool: 'inline-source-map',
+  devServer: {
+        contentBase: './dist'
+      },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output YYYYY'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
