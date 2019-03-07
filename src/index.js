81d2450baf0205660138be376fdd9d6ddd556ce7
ben <moon791017@gmail.com>
2019-03-07 13:58:12 +0800

    OK

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { draw } from './CustomThreeJS/CreateView.js'
 ((d,w) => {
 
   const component = () => {
-    let element = document.createRange().createContextualFragment(` <div id="loading-overlay" class="loading-overlay-hidden">
+    let element = d.createRange().createContextualFragment(` <div id="loading-overlay" class="loading-overlay-hidden">
     <div id="loading-bar">
         <span id="progress">      
         </span>
@@ -18,10 +18,13 @@ import { draw } from './CustomThreeJS/CreateView.js'
     return element;
   }
 
-  document.body.appendChild(component());
+  d.body.appendChild(component());
   let queryString = w.location.search.split("=")[1];
-  if(queryString !== undefined || queryString !== "" ){
+  if(queryString !== undefined && queryString !== "" ){
+    console.log(queryString)
     draw(queryString);
+  }else{
+    draw("https://raw.githubusercontent.com/jscad/sample-files/fe410b44bca54c2194e17d5deb544854e14e82ec/stl/UM2CableChain_BedEnd.STL")
   }
   // example url 
   // https://raw.githubusercontent.com/jscad/sample-files/fe410b44bca54c2194e17d5deb544854e14e82ec/stl/UM2CableChain_BedEnd.STL
